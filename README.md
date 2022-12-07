53d4e571f975928c02554df0812463c9c7227155
poolodoo <49055484+poolodoo@users.noreply.github.com>
2022-12-07 09:55:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-useRef-3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-sxvnmv)
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./style.css";
+
+export default function App() {
+  // the ref that will connect to input field
+  const nameRef = useRef("Sun");
+
+  // form submit handler
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      // every ref will ALWAYS have the "current" property, which is the HTML DOM element
+      userName: nameRef.current.value
+    });
+  }
+  return (
+    <div>
+      <h2>Forms and Input Fields in React with useRef</h2>
+      <form onSubmit={onSubmit}>
+        <input id="name" ref={nameRef} type="text" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
